a9b7cb41f0fcaf3e124873db4dc2cbc978e84c0b
KlausDrown <klausdrown@gmail.com>
2024-07-21 19:04:43 +0300

    poputka

---
+++ LeetCode.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "LongestConsecutiveSequence.h"
+
+int main() {
+	LongestConsecutiveSequence solut;
+	vector<int> nums{ 2,20,4,10,3,4,5 };
+	cout << "Function return = ";
+	cout << solut.longestConsecutive(nums);
+	return 0;
+}
+
---
+++ LongestConsecutiveSequence.cpp
@@ -0,0 +1,1 @@
+#include "LongestConsecutiveSequence.h"
