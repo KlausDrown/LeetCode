4661ca854929c7ca134ff448cdc23101d2d1a569
KlausDrown <klausdrown@gmail.com>
2024-07-23 23:51:28 +0300

    ruki ne ottuda

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,11 +1,24 @@
-﻿#include <iostream>
+﻿#include <vector>
+#include <iostream>
 #include "LongestConsecutiveSequence.h"
+#include "ThreeSum.h"
+using namespace std;
+
+
+
+
+
+
+
+
+
+
 
 int main() {
-	LongestConsecutiveSequence solut;
-	vector<int> nums{ 2,20,4,10,3,4,5 };
-	cout << "Function return = ";
-	cout << solut.longestConsecutive(nums);
-	return 0;
+    vector<int> nums{ -1,0,1,2,-1,-4 };
+    ThreeSum suma;
+    suma.sum3(nums);
+   
+    return 0;
 }
 
--- LongestConsecutiveSequence.h
+++ LongestConsecutiveSequence.h
@@ -1,59 +1,59 @@
-#include<iostream>
-#include <vector>
-#include <set>
-#include <set>
-#include <unordered_set>
-using namespace std;
-/* https://neetcode.io/problems/longest-consecutive-sequence */
-class LongestConsecutiveSequence 
-{
-public:
-	int longestConsecutive(vector<int>& nums) {
-		//переменные
-		vector<int> newmass;
-		int MAXposled=0;
-		int Vrevposled = 0;
-		int SIZEnums = nums.size();
-		unordered_set<int> nums2;
-		//переменные
-		for (int i = 0; i < SIZEnums; i++)
-		{
-			nums2.insert(nums[i]);
-		}
-		for (int i : nums2)
-		{
-			cout << i << endl;
-			if (!(nums2.count(i - 1)))
-			{
-				newmass.push_back(i);
-			}
-		}
-		cout << "---------------------------------------" << endl << endl;
-		for (int i : newmass)
-		{
-			cout << i << endl;
-		}
-		for (int i = 0; i < newmass.size(); i++)
-		{
-			int j = 0;
-			while (true)
-			{
-				j++;
-				if (nums2.count(newmass[i] + j))
-				{
-					Vrevposled += +1;
-				}
-				else
-				{
-					if (MAXposled <= Vrevposled)MAXposled = Vrevposled;
-					break;
-				}
-			}
-		}
-
-		return (MAXposled + 1);
-	}
-
-};
-
-
+//#include<iostream>
+//#include <vector>
+//#include <set>
+//#include <set>
+//#include <unordered_set>
+//using namespace std;
+///* https://neetcode.io/problems/longest-consecutive-sequence */
+//class LongestConsecutiveSequence 
+//{
+//public:
+//	int longestConsecutive(vector<int>& nums) {
+//		//переменные
+//		vector<int> newmass;
+//		int MAXposled=0;
+//		int Vrevposled = 0;
+//		int SIZEnums = nums.size();
+//		unordered_set<int> nums2;
+//		//переменные
+//		for (int i = 0; i < SIZEnums; i++)
+//		{
+//			nums2.insert(nums[i]);
+//		}
+//		for (int i : nums2)
+//		{
+//			cout << i << endl;
+//			if (!(nums2.count(i - 1)))
+//			{
+//				newmass.push_back(i);
+//			}
+//		}
+//		cout << "---------------------------------------" << endl << endl;
+//		for (int i : newmass)
+//		{
+//			cout << i << endl;
+//		}
+//		for (int i = 0; i < newmass.size(); i++)
+//		{
+//			int j = 0;
+//			while (true)
+//			{
+//				j++;
+//				if (nums2.count(newmass[i] + j))
+//				{
+//					Vrevposled += +1;
+//				}
+//				else
+//				{
+//					if (MAXposled <= Vrevposled)MAXposled = Vrevposled;
+//					break;
+//				}
+//			}
+//		}
+//
+//		return (MAXposled + 1);
+//	}
+//
+//};
+//
+//
