38b29ed627d06f61539d025a973c5c490d240f95
KlausDrown <klausdrown@gmail.com>
2024-07-29 18:42:32 +0300

    состояние пациента стабильное все 22 ошибки решены путём созда- ниянового класса(ListNode) который является списком.

---
+++ ListNide.h
@@ -0,0 +1,13 @@
+#pragma once
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
--- MergeTwoSortedLinkedLists.h
+++ MergeTwoSortedLinkedLists.h
@@ -1,22 +1,35 @@
-
-
+#include <vector>
+#include <iostream>
+#include "ListNide.h"
 using namespace std;
 
 
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+
  
 
 class MergeTwoSortedLinkedLists
 {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* mergeTwoLists(ListNode* list1 /*ListNode* list2*/) {
+        ListNode* NEXT = NULL;
+        ListNode* curr(0);
+        ListNode* ToRETURN = curr;
 
+        for(int i=0; i <4; i++)
+        {
+            if (true/*list1->val > list2->val*/)
+            {
+                curr->next = list1;
+                curr = list1;
+                list1 = list1->next;
+                
+            }
+            else
+            {
+
+            }
+        }
+        return curr;
     }
 };
 
