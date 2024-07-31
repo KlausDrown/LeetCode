06e10edd6e293dad635bbe9223d4ffbf4cf1b779
KlausDrown <klausdrown@gmail.com>
2024-07-31 08:05:41 +0300

    временно перетащил класс

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,13 +1,59 @@
 ﻿#include <vector>
 #include <iostream>
-#include "MyLibrary\LinkedList\MergeTwoSortedLinkedLists.h"
-#include "MyLibrary\LinkedList\ListNide.h"
+//#include "MyLibrary\Task5_MinStack\MinStack.h"
 using namespace std;
 
 //Definition for singly-linked list.
 
 
+class MinStack {
+private:
+    vector<long int> MyStack;
+    vector<long int> minval;
+    bool NewExit = true;
+public:
 
+    //MinStack() {  
+
+    //};
+    void push(int vallue) {
+        MyStack.push_back(vallue);
+        if (NewExit) minval.push_back(MyStack.back());
+
+        if (minval.back() >= MyStack.back() && !NewExit)
+        {
+            minval.push_back(MyStack.back());
+        }
+        NewExit = false;
+
+
+
+    }
+
+    void pop() {
+        auto iter = MyStack.cbegin();
+        auto iter2 = minval.cbegin();
+        if (minval.back() == MyStack.back())
+        {
+            minval.erase(iter2 + minval.size() - 1);
+            auto iter2 = minval.cbegin();
+            MyStack.erase(iter + MyStack.size() - 1);
+            auto iter = MyStack.cbegin();
+        }
+        else {
+            MyStack.erase(iter + MyStack.size() - 1);
+            auto iter = MyStack.cbegin();
+        }
+    }
+
+    int top() {
+        return MyStack.back();
+    }
+
+    int getMin() {
+        return minval.back();
+    }
+};
 
 
 
@@ -17,6 +63,23 @@ using namespace std;
 
 
 int main() {
+    MinStack* minStack = new MinStack();
+    minStack->push(2147483646);
+    minStack->push(2147483646);
+    minStack->push(2147483647);
+    minStack->top();
+    minStack->pop();
+    minStack->getMin();
+    minStack->pop();
+    minStack->push(2147483647);
+    minStack->top();
+    minStack->getMin();
+    minStack->push(-2147483648);
+    minStack->top();
+    minStack->getMin();
+    minStack->pop();
+    minStack->getMin();
+
 
     return 0;
 }
