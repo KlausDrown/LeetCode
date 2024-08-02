e34917e6027f9adef88196504dfd11db96acaab8
KlausDrown <klausdrown@gmail.com>
2024-08-02 08:22:16 +0300

    Скопировал структуру деревьев с LeedCode данная ветвь была создана, чтобы один раз создать структуру и забыть о ней не трогая main. данная ветвь будет основой для работы с кластером задач про деревья.

---
+++ MyLibrary/Tree/Tree.h
@@ -0,0 +1,9 @@
+struct TreeNode { /* For working with trees */
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+	
+};
