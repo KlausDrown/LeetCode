77b13be9e837078c7ca04c5c672004e296f1b385
KlausDrown <klausdrown@gmail.com>
2024-08-02 03:36:53 +0300

    рабочее, но не оптимизированое решение.

--- MyLibrary/SimpleTask1_BuyAndSell/BuyAndSell.h
+++ MyLibrary/SimpleTask1_BuyAndSell/BuyAndSell.h
@@ -1,9 +1,33 @@
-#pragma once
+#include<vector>
+#include<iostream>
+using namespace std;
+
 class BuyAndSell
 {
 public:
+    int profit = 0;
     int maxProfit(vector<int>& prices) {
+        int SmallestNumb = 0;
+        for (int i = 0; i < prices.size()-1; i++)
+        {
+            if (prices[SmallestNumb] >= prices[i]) SmallestNumb = i;
+            int iter = i+1;
+            if (prices[i]<prices[i + 1])
+            {
+                int MaxNumb = prices[i + 1];
+                while ((prices[SmallestNumb] < prices[iter]) && (iter != prices.size()))
+                {
+                    if (MaxNumb < prices[iter]) MaxNumb = prices[iter];
+                    iter++;
+                    if (iter == prices.size()) break;
+                }
+                if (profit < (MaxNumb - prices[SmallestNumb])) profit = (MaxNumb - prices[SmallestNumb]);
 
+            }
+            
+        }
+        cout << profit << endl;
+        return profit;
     }
 };
 
